6571d3662576b150b632a395ed9ac7fff136d50e
sharmavikas4 <vikassharma20015@gmail.com>
2025-12-17 10:39:25 +0530

    Changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Climafix",
+  title: "Climafix Summit",
   description: "Climafix Summit 2025 - Innovating for a Sustainable Future",
 };
 
